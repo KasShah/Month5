238dd34c2cf60fee502275565525ebdd07f3ec0a
KasShah <kasievashahi599@gmail.com>
2024-01-23 10:27:28 +0600

    'HW3'

--- movie_app/views.py
+++ movie_app/views.py
@@ -6,49 +6,89 @@ from .models import Director, Movie, Review
 from .serializers import DirectorSerializer, MovieSerializer, ReviewSerializer
 
 
-@api_view(['GET', 'PUT'])
+@api_view(['GET', 'POST'])
 def directors_api_view(request):
     if request.method == 'GET':
         queryset = Director.objects.all()
         serializer = DirectorSerializer(queryset, many=True)
         return Response(serializer.data, status=200)
-
-
-@api_view(['GET'])
-def directors_detail_api_view(request, id):
-    if request.method == 'GET':
-        queryset = get_object_or_404(Director, id=id)
-        serializer = DirectorSerializer(queryset, many=False)
+    elif request.method == 'POST':
+        serializer = DirectorSerializer(data=request.data)
+        serializer.is_valid(raise_exception=True)
+        serializer.save()
         return Response(serializer.data, status=200)
 
 
-@api_view(['GET'])
+@api_view(['GET', 'PUT', 'DELETE'])
+def directors_detail_api_view(request, id):
+    queryset = get_object_or_404(Director, id=id)
+    if request.method == 'GET':
+        serializer = DirectorSerializer(queryset, many=False)
+        return Response(serializer.data, status=200)
+    elif request.method == 'PUT':
+        serializer = DirectorSerializer(queryset, data=request.data)
+        serializer.is_valid(raise_exception=True)
+        serializer.save()
+        return Response(serializer.data, status=200)
+    elif request.method == 'DELETE':
+        queryset.delete()
+        return Response(status=204)
+
+
+
+@api_view(['GET', 'POST'])
 def movies_api_view(request):
     if request.method == 'GET':
         queryset = Movie.objects.all()
         serializer = MovieSerializer(queryset, many=True)
         return Response(serializer.data, status=200)
-
-
-@api_view(['GET'])
-def movies_detail_api_view(request, id):
-    if request.method == 'GET':
-        queryset = get_object_or_404(Director, id=id)
-        serializer = MovieSerializer(queryset, many=False)
+    elif request.method == 'POST':
+        serializer = MovieSerializer(data=request.data)
+        serializer.is_valid(raise_exception=True)
+        serializer.save()
         return Response(serializer.data, status=200)
 
 
-@api_view(['GET'])
+@api_view(['GET', 'PUT', 'DELETE'])
+def movies_detail_api_view(request, id):
+    queryset = get_object_or_404(Director, id=id)
+    if request.method == 'GET':
+        serializer = MovieSerializer(queryset, many=False)
+        return Response(serializer.data, status=200)
+    elif request.method == 'PUT':
+        serializer = MovieSerializer(queryset, data=request.data)
+        serializer.is_valid(raise_exception=True)
+        serializer.save()
+        return Response(serializer.data, status=200)
+    elif request.method == 'DELETE':
+        queryset.delete()
+        return Response(status=204)
+
+
+@api_view(['GET', 'POST'])
 def reviews_api_view(request):
     if request.method == 'GET':
         queryset = Review.objects.all()
         serializer = ReviewSerializer(queryset, many=True)
         return Response(serializer.data, status=200)
+    elif request.method == 'POST':
+        serializer = DirectorSerializer(data=request.data)
+        serializer.is_valid(raise_exception=True)
+        serializer.save()
+        return Response(serializer.data, status=200)
 
 
-@api_view(['GET'])
+@api_view(['GET', 'PUT', 'DELETE'])
 def reviews_detail_api_view(request, id):
+    queryset = get_object_or_404(Director, id=id)
     if request.method == 'GET':
-        queryset = get_object_or_404(Director, id=id)
         serializer = ReviewSerializer(queryset, many=False)
         return Response(serializer.data, status=200)
+    elif request.method == 'PUT':
+        serializer = ReviewSerializer(queryset, data=request.data)
+        serializer.is_valid(raise_exception=True)
+        serializer.save()
+        return Response(serializer.data, status=200)
+    elif request.method == 'DELETE':
+        queryset.delete()
+        return Response(status=204)
